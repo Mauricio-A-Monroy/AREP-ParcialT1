37741fe33e78486cad80cb22a5b78e7a57bac3d3
unknown <mauricio.monroy-j@labinfo.is.escuelaing.edu.co>
2025-02-21 10:48:06 -0500

    1

--- src/main/java/edu/escuelaing/arep/parcial/FacadeServer.java
+++ src/main/java/edu/escuelaing/arep/parcial/FacadeServer.java
@@ -1,17 +1,20 @@
 package edu.escuelaing.arep.parcial;
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 
-public class FacadeServer
-{
+public class FacadeServer {
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL ="http://localhost:8080";
     public static void main( String[] args ) throws IOException {
         ServerSocket serverSocket = null;
         try {
             serverSocket = new ServerSocket(36000);
         } catch (IOException e) {
-            System.err.println("Could not listen on port: 35000.");
+            System.err.println("Could not listen on port: 36000.");
             System.exit(1);
         }
 
@@ -45,15 +48,55 @@ public class FacadeServer
 
             System.out.println("Path: " + path);
 
-            outputLine = "HTTP/1.1 200 OK\r\n"
-                    + "Content-Type: text/html\r\n"
-                    + "\r\n" + getDefaultResponse();
+            if (path.startsWith("/cliente ")) {
+                outputLine = "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: text/html\r\n"
+                        + "\r\n" + getDefaultResponse();
+            }
+            else if(path.startsWith("/consulta")){
+                outputLine = connectToBackEnd(path);
+            }
+            else{
+                outputLine = "HTTP/1.1 404 NOT FOUND\r\n"
+                        + "Content-Type: text/html\r\n"
+                        + "\r\n";
+            }
             out.println(outputLine);
             out.close();
             in.close();
             clientSocket.close();
         }
         serverSocket.close();
+    }
+
+    private static String connectToBackEnd(String path) throws IOException {
+        URL obj = new URL(GET_URL + path.replace("consulta", "compreflex"));
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            return response.toString();
+        } else {
+            return "HTTP/1.1 404 NOT FOUND\r\n"
+                    + "Content-Type: text/html\r\n"
+                    + "\r\n";
+        }
+
     }
 
     private static String getDefaultResponse() throws IOException {
